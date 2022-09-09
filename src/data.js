1c746db247957c73716d2196bfa7010044372645
Akshay1675 <112275432+Akshay1675@users.noreply.github.com>
2022-09-09 16:58:10 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,114 @@
+let shopItemsData = [
+    {
+        id: "jjdjdjdj",
+        name: "Onion",
+        price: 28,
+        desc: "Organic onion 1 Kg",
+        img: "images/40023472_5-fresho-onion-organically-grown.webp"
+    },
+    {
+        id: "akskdfdf",
+        name: "Tomato",
+        price: 19,
+        desc: "Tomato - Organically Grown",
+        img: "images/40022638_4-fresho-tomato-local-organically-grown.webp"
+    },
+    {
+        id: "ddkksksk",
+        name: "Garlic",
+        price: 19,
+        desc: "Garlic - Organically Grown",
+        img: "images/50000507_5-fresho-garlic-organically-grown.webp"
+    },
+    {
+        id: "dkssiaksnbehf",
+        name: "Amla",
+        price: 49,
+        desc: "Amla - Organically Grown",
+        img: "images/40004383_12-fresho-amla.webp"
+    },
+    {
+        id: "dkssisnbehf",
+        name: "Carrot",
+        price: 40,
+        desc: "Carrot - Organically Grown",
+        img: "images/40022634_6-fresho-carrot-organically-grown.webp"
+    },
+    {
+        id: "dkssisnbvfehf",
+        name: "Coconut",
+        price: 19,
+        desc: "Coconut - large",
+        img: "images/10000093_16-fresho-coconut-medium.webp"
+    },
+    {
+        id: "dkssisnbekl",
+        name: "Brinjal",
+        price: 49,
+        desc: "Brinjal - Organically Grown",
+        img: "images/10000058_20-fresho-brinjal-varikatri.webp"
+    },
+    {
+        id: "dkssisnbegh",
+        name: "Ghee",
+        price: 250,
+        desc: "Ghee ",
+        img: "images/1212447_2-fresho-organic-cow-desi-gheetuppa.webp"
+    },
+    {
+        id: "dkssisnbad",
+        name: "oil",
+        price: 199,
+        desc: "sunflower oil ",
+        img: "images/1204455_2-fortune-sunflower-refined-oil-sun-lite.webp"
+    },
+    {
+        id: "dkssisneefhh",
+        name: "oil",
+        price: 250,
+        desc: "groundnut oil ",
+        img: "images/40013920_5-mr-gold-groundnut-oil.webp"
+    },
+    {
+        id: "dkssisnbvft",
+        name: "Beans",
+        price: 40,
+        desc: "Beans - Organically Grown",
+        img: "images/40023469_7-fresho-beans-organically-grown.webp"
+    },
+    {
+        id: "dkssbkvbehf",
+        name: "Capsicum",
+        price: 80,
+        desc: "Capsicum - Organically Grown",
+        img: "images/50000479_5-fresho-capsicum-green-organically-grown.webp"
+    },
+    {
+        id: "dkssiskkbkehf",
+        name: "Salt",
+        price: 25,
+        desc: "Salt  ",
+        img: "images/241600_5-tata-salt-iodized.webp"
+    },
+    {
+        id: "dkssikvjgbehf",
+        name: "Rice",
+        price: 499,
+        desc: "Rice ",
+        img: "images/204005_7-daawat-basmati-rice-rozana-gold.webp"
+    },
+    {
+        id: "dkssisnjtbff",
+        name: "Suger",
+        price: 35,
+        desc: "Suger ",
+        img: "images/100342220_5-parrys-pure-refined-sugar-sulphur-free.webp"
+    },
+    {
+        id: "dkssisnsjff",
+        name: "Atta",
+        price: 400,
+        desc: "Atta 10kg",
+        img: "images/126906_8-aashirvaad-atta-whole-wheat.webp"
+    },
+  ];
